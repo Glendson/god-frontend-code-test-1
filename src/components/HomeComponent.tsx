5079ab2a49f487c02c1bcaa0256777f1b80a074b
Glendson <gztomazetto@gmail.com>
2024-01-31 14:50:22 +0000

    feat: add homepage component

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { Text } from "vcc-ui";
+
+import styles from '../../public/css/home.module.css'
+
 import { useCars } from "../hooks/useCars";
 import { CarCard } from "./CarCard";
+import { Spacer } from "./Spacer";
 
 export const HomeComponent: React.FC = () => {
 
   const { cars } = useCars()
 
-  console.log(cars)
-
   return (
-    <div>
-      {cars.map(car => <CarCard key={car.id} car={car}/>)}
+    <div className={styles.homeWrapper}>
+      <Text variant="cook">Todos os modelos Recharge</Text>
+      <Spacer></Spacer>
+      <div className={styles.cardsWrapper} id="card-list">
+        {cars.map(car => <CarCard key={car.id} car={car}/>)}
+      </div>
+      
     </div>    
   );
 };
